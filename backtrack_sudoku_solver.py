342ab7eec5ac4266c5e7803b7bb916d2be2fe774
elpython3 <elpython3@gmail.com>
2023-06-27 22:00:35 -0700

    Comments

--- backtrack_sudoku_solver.py
+++ backtrack_sudoku_solver.py
@@ -1,16 +1,35 @@
-demo_board = [
-    [[9], [5], [7]], [[], [1], [3]], [[2], [8], [4]],
-    [[4], [], [3]], [[], [], [7]], [[1], [9], [6]],
-    [[6], [], [2]], [[], [4], []], [[5], [], []],
-    [[], [7], []], [[], [], []], [[], [], []],
-    [[], [], []], [[9], [], [1]], [[], [], []],
-    [[], [], []], [[], [2], []], [[], [4], []],
-    [[], [], [5]], [[], [], []], [[6], [], [3]],
-    [[], [9], []], [[4], [], []], [[], [7], []],
-    [[7], [], [6]], [[], [], []], [[], [], []]
-]
+# author: @elpython3
 
-# Helper functions to get the row, column, and quadrant idx in the bo format.
+# NOTICE:
+# For those wanting to try out this script, make sure your sudoku board is formatted into a list such as the following:
+# 
+# empty_board = [
+#     [[], [], []], [[], [], []], [[], [], []],
+#     [[], [], []], [[], [], []], [[], [], []],
+#     [[], [], []], [[], [], []], [[], [], []],
+#     [[], [], []], [[], [], []], [[], [], []],
+#     [[], [], []], [[], [], []], [[], [], []],
+#     [[], [], []], [[], [], []], [[], [], []],
+#     [[], [], []], [[], [], []], [[], [], []],
+#     [[], [], []], [[], [], []], [[], [], []],
+#     [[], [], []], [[], [], []], [[], [], []]
+# ]
+# 
+# An example would look like this:
+
+# demo_board = [
+#     [[9], [5], [7]], [[], [1], [3]], [[2], [8], [4]],
+#     [[4], [], [3]], [[], [], [7]], [[1], [9], [6]],
+#     [[6], [], [2]], [[], [4], []], [[5], [], []],
+#     [[], [7], []], [[], [], []], [[], [], []],
+#     [[], [], []], [[9], [], [1]], [[], [], []],
+#     [[], [], []], [[], [2], []], [[], [4], []],
+#     [[], [], [5]], [[], [], []], [[6], [], [3]],
+#     [[], [9], []], [[4], [], []], [[], [7], []],
+#     [[7], [], [6]], [[], [], []], [[], [], []]
+# ]
+
+# Helper functions to get the row, column, and quadrant idx in the board format.
 def row_idx(i):
     if (i+1) <= 3:
         return 0
@@ -111,8 +130,8 @@ def get_quadrant(bo, idx):
                     quadrant.append(0)
     return quadrant
 
-# Function to print the bo in a cleaner format
-def print_board(bo):
+# Function to print the board in a cleaner format, this one is mostly used for debug.
+def print_board_debug(bo):
     print(f"{bo[0]}, {bo[1]}, {bo[2]},")
     print(f"{bo[3]}, {bo[4]}, {bo[5]},")
     print(f"{bo[6]}, {bo[7]}, {bo[8]},")
@@ -123,6 +142,19 @@ def print_board(bo):
     print(f"{bo[21]}, {bo[22]}, {bo[23]},")
     print(f"{bo[24]}, {bo[25]}, {bo[26]}\n")
 
+# Function to print the board in a cleaner format, this one more fancy than the former.
+def print_board(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[i])):
+            if (j+1) % 3 == 0:
+                if (i+1) % 3 != 0:
+                    print(f"{bo[i][j][0]} |", end=" ")
+                else:
+                    print(f"{bo[i][j][0]}")
+            else:
+                print(f"{bo[i][j][0]} ", end="")
+        if (i+1) % 9 == 0 and i < 26:
+            print("---------------------")
 
 
 def solve(bo):
@@ -132,16 +164,26 @@ def solve(bo):
     found_i, found_j = found_empty
     
     for i in range(1, 10):
+        # Check if a number from 1-9 works as a possible solution.
         if validate(bo, i, (found_i, found_j)):
+            # It works so far, set the square as the value of i
             bo[found_i][found_j] = [i]
+
+            # Implementing a bactracking algorithim.
+            # Backtracking is a recursive algorithm (meaning it involves a function calling itself). In this case, the function will always call itself
+            # until either the entire board is solved, or a space creates an impossible solution (eg. duplicates in a row). If the former occurs, the
+            # function will thus completely stop, as all instances of the function will return True (see line 176). If the latter occurs, the function
+            # instance will stop, and keep going to previous instances until another possible solution occurs (thus called a backtracking algorithm).
             if solve(bo):
-                return True            
+                return True
+            
+            # It doesn't work, reset the value to empty
             bo[found_i][found_j] = []
 
+    # Occurs if an impossible solution occurs.
     return False
 
 def validate(bo, num, pos):
-    # row_num = row_idx(pos[0])
     row = get_row(bo, row_idx(pos[0]))
     for i in range(9):
         if row[i] == num:
@@ -156,7 +198,7 @@ def validate(bo, num, pos):
     for i in range(9):
         if quadrant[i] == num:
             return False
-    
+
     return True
 
 def find_empty(bo):
@@ -166,6 +208,19 @@ def find_empty(bo):
                 return (i, j)
     return None
 
-counter = 0
-solve(demo_board)
-print_board(demo_board)
+
+if __name__ == '__main__':
+    demo_board = [
+        [[9], [5], [7]], [[], [1], [3]], [[2], [8], [4]],
+        [[4], [], [3]], [[], [], [7]], [[1], [9], [6]],
+        [[6], [], [2]], [[], [4], []], [[5], [], []],
+        [[], [7], []], [[], [], []], [[], [], []],
+        [[], [], []], [[9], [], [1]], [[], [], []],
+        [[], [], []], [[], [2], []], [[], [4], []],
+        [[], [], [5]], [[], [], []], [[6], [], [3]],
+        [[], [9], []], [[4], [], []], [[], [7], []],
+        [[7], [], [6]], [[], [], []], [[], [], []]
+    ]
+    solve(demo_board)
+    print_board(demo_board)
+    # print_board_debug(demo_board)
