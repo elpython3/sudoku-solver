f4a8a267da5a715a8c0db000cf14db52fc6491dd
elpython3 <elpython3@gmail.com>
2023-07-01 15:47:33 -0700

    Incomplete 'play' page

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 
 import backtrack_sudoku_solver as bt_ss
 import board_generator
@@ -28,16 +28,41 @@ def solve_board_from_table():
     solved_board = bt_ss.format_back(formatted_board)
     return solved_board
 
+def is_valid(i, j):
+    board = []
+    for i in range(9):
+        subset = []
+        for j in range(9):
+            cell_name = f'cell-{i}-{j}'
+            try:
+                cell_value = int(request.form.get(cell_name, '0'))
+                subset.append(cell_value)
+            except ValueError:
+                subset.append(None)
+        board.append(subset)
+
+    if board[i][j] == session['full_board'][i][j]:
+        return True
+    else:
+        return False
+
 @app.route("/", methods=['POST', 'GET'])  # this sets the route to this page
 def home():
     board = board_generator.gen_board()
+
+    if 'full_board' not in session:
+        session['full_board'] = board
+    
+    if 'incorrect_list' not in session:
+        session['incorrect_list'] = []
 
     for i in range(9):
         for j in range(9):
             if board[i][j] == 0:
                 board[i][j] = None
     
-    return render_template("home.html", board=board)
+    return render_template("home.html", board=board, incorrect_list=[])
+    # return redirect("/solver")
 
 @app.route("/solver", methods=['POST', 'GET'])  # this sets the route to this page
 def solver():
@@ -49,6 +74,10 @@ def solver():
                 board[i][j] = None
 
     return render_template("solver.html", board=board)
+
+@app.route("/gen-play", methods=['POST', 'GET'])
+def gen_play():
+    return redirect("/")
 
 @app.route("/gen", methods=['POST', 'GET'])
 def generate():
@@ -62,8 +91,8 @@ def solve():
 
 @app.route('/check', methods=['POST', 'GET'])
 def check():
-    full_board = solve_board_from_table()
-
+    full_board = session['full_board']
+    
     board = []
     for i in range(9):
         subset = []
@@ -80,14 +109,14 @@ def check():
     for i in range(9):
         for j in range(9):
             if full_board[i][j] == board[i][j]:
-                print("Correct. Moving on.")
+                # print("Correct. Moving on.")
                 continue
             else:
                 if board[i][j] == 0 or board[i][j] == None:
-                    print("This square is empty. Moving on.")
+                    # print("This square is empty. Moving on.")
                     continue
                 else:
-                    print("Incorrect")
+                    # print("Incorrect")
                     incorrect_list.append((bt_ss.row_idx(i), bt_ss.column_idx(i, j)))
     print(incorrect_list)
     if len(incorrect_list) > 0:
@@ -97,8 +126,9 @@ def check():
         for j in range(9):
             if board[i][j] == 0:
                 board[i][j] = None
-
-    return render_template("home.html", board=board)
+    session['incorrect_list'] = incorrect_list
+    return render_template("home.html", board=board, incorrect_list=incorrect_list)
+    # return redirect("/")
 
 @app.route("/clear", methods=['POST'])
 def clearer():
